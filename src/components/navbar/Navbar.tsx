cf7508a242726bf3a04dcb2e95394cf0d3c012ea
samuelsadev <samuelsa.dev@gmail.com>
2024-08-16 12:50:05 -0300

    criando header

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,25 @@
 import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div className='flex gap-4 nb-4'>
-    <h1>Navbar</h1>
-    <Link to='/home'>Home</Link>
+    <div className="flex gap-4 mb-4">
+      <ul className="flex gap-4 relative menu">
+        <Link to="/home">
+          <li style={{  }} className="text-lg font-medium cursor-pointer hover:text-zinc-500 hover:scale-110 transition-all">Home</li>
+        </Link>
+        <Link to="/home">
+          <li className="text-lg font-medium cursor-pointer hover:text-zinc-500 hover:scale-110 transition-all">Produtos</li>
+        </Link>
+        <Link to="/home">
+          <li className="text-lg font-medium cursor-pointer hover:text-zinc-500 hover:scale-110 transition-all">Sobre</li>
+        </Link>
+        <Link to="/home">
+          <li className="text-lg font-medium cursor-pointer hover:text-zinc-500 hover:scale-110 transition-all">Contato</li>
+        </Link>
+      </ul>
     </div>
+
   )
 }
 
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import Navbar from '../components/navbar/Navbar'
+import logo from '../../src/assets/logo.svg'
 
 function Header() {
   return (
-    <header>
-        header 
-        <Navbar />
+    <header className="max-w-7xl flex items-center justify-between p-4 mx-auto">
+      <img className="w-44" src={logo} alt="logo" />
+      <Navbar />
     </header>
   )
 }
