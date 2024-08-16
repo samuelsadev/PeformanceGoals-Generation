5e7ec726cf1ebf7df409e149f122073145c41966
samuelsadev <samuelsa.dev@gmail.com>
2024-08-16 12:01:10 -0300

    criando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Home from './components/home/Home'
+import Header from './header/Header'
 
 function App() {
   return (
-    <div className='bg-red-500'>App</div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div className='flex gap-4 nb-4'>
+    <h1>Navbar</h1>
+    <Link to='/home'>Home</Link>
+    </div>
+  )
+}
+
+export default Navbar
